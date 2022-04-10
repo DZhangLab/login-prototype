437e90882b0b7e8cb8e710556766b5a633db30e3
Minghao Zhao <minghaozhao@Minghaos-MacBook-Air.local>
2022-04-10 00:01:49 -0400

    The prototype is switched to MongoDB. There are still problems with twitter login, and a bug for firsttime storing the googleID and facebookID to DB

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const salt = bcrypt.genSaltSync(10);
 
 const mysql = require("mysql");
 
+const ejs = require("ejs");
+
 const session = require("express-session");
 
 const passport = require("passport");
@@ -27,6 +29,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const FacebookStrategy = require("passport-facebook");
 
+const TwitterStrategy = require("passport-twitter");
+
 const LocalStrategy = require('passport-local').Strategy;
 
 const accountSid = process.env.TWILLOSID;
@@ -34,6 +38,8 @@ const accountSid = process.env.TWILLOSID;
 const authToken = process.env.TWILLOWTOKEN;
 
 const client = require('twilio')(process.env.TWILLOSID, process.env.TWILLOWTOKEN);
+
+const findOrCreate = require("mongoose-findorcreate");
 
 const jsdom = require("jsdom");
 const {
@@ -68,201 +74,94 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
-// connect to MySQL serve, the user and password should be replaced in the future when connecting to cloud database
-const con = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "root",
-  database: "userdb"
+mongoose.connect("mongodb://localhost:27017/loginDB");
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  googleID: String,
+  facebookId: String,
+  sentTime: String,
+  verCode: String,
+  password: String
 });
 
-// connecting to sql server
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("connected");
-  con.query("CREATE DATABASE IF NOT EXISTS userdb", function(err, result) {
-    if (err) throw err;
-    console.log("database created");
-  });
-  const sqlQuery = "CREATE TABLE IF NOT EXISTS login_info (email varchar(30), firstName varchar(30), lastName varchar(30), phoneNumber varchar(30), password varchar(30))"
-  con.query(sqlQuery, function(err, result) {
-    if (err) {
-      console.log(err);
-    };
-    console.log("login info table created");
-  });
-});
-// const user1 = new User({name: "harry", password: "password"});
-//
-// user1.save();
-// =========================================================================
-// passport session setup ==================================================
-// =========================================================================
-// required for persistent login sessions
-// passport needs ability to serialize and unserialize users out of session
-// used to serialize the user for the session
-// passport.use(User.createStrategy());
-//
-// passport.serializeUser(function(user, done) {
-//     done(null, user.id);
-//    // where is this user.id going? Are we supposed to access this anywhere?
-// });
-//
-// // used to deserialize the user
-// passport.deserializeUser(function(id, done) {
-//     User.findById(id, function(err, user) {
-//         done(err, user);
-//     });
-// });
-//
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-// create customer fields
-const customeFields = {
-  usernameField: "uname",
-  passowrdFiled: "pw"
-};
 
-// create verifycallback function to get the user info from the database
-const verifyCallback = (username, password, done) => {
-  con.query("SELECT * FROM login_info WHERE email = ?", [username], function(err, results, fields) {
-    if (err)
-      return done(err);
-    if (results.length === 0) { // the user is not in the database
-      return done(null, false);
-    }
-    const isValid = validPassword(password, results[0].hash);
-    user = {
-      email: results[0].email,
-      hash: results[0].hash,
-      salt: results[0].salt
-    };
-    if (isValid) {
-      return done(null, user);
-    } else {
-      return done(null, false);
-    }
-  });
-}
+const User = mongoose.model("User", userSchema);
 
-// initialize local strategy
-const strategy = new LocalStrategy(customeFields, verifyCallback);
-passport.use(strategy);
+passport.use(User.createStrategy());
 
-// use to serialize the user
+
+
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user.id);
+   // where is this user.id going? Are we supposed to access this anywhere?
 });
 
 // used to deserialize the user
 passport.deserializeUser(function(id, done) {
-  con.query("SELECT * FROM login_info WHERE facebook_id = '" + id + "' OR google_id = '" + id + "'", function(err, rows) {
-    done(err, rows[0]);
-  });
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
 });
-
-// create the function validpassword to validate the password
-function validPassword(password, hash) {
-  return bcrypt.compare(password, hash)
-}
-
-// create the function to genreate the hash for password
-function genPassword(password) {
-  const salt = bcrypt.genSaltSync(10);
-  bcrypt.hash(password, salt, function(err, hash) {
-    if (err) {
-      console.log(err);
-    }
-    return {
-      salt: salt,
-      hash: hash
-    }
-  });
-}
-
-// function that chech if the session is authenticated
-function isAuth(req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.send("not authorized");
-  }
-}
-
-// function that check if the user exists
-function userExists(req, res, next) {
-  con.query("SELECT * FROM login_info WHERE email = ?", [req.body.email], function(err, results, fields) {
-    if (err) {
-      console.log("err")
-    } else if (results.length > 0) {
-      res.send("user exists");
-    } else {
-      next();
-    }
-  });
-}
-
-// call the google strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    scope: ['https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/userinfo.email'
-    ]
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    const findUser = "SELECT google_id FROM login_info WHERE google_id = '" + profile.id + "'";
-    //console.log(findUser);
-    con.query(findUser, function(err, result, field) {
-      if (err) throw err;
-      console.log(result);
-      if (result.length === 0) {
-        con.query("INSERT INTO login_info (google_id) VALUES ('" + profile.id + "')", function(err, result, field) {
-          if (err) throw err;
-        });
-      } else {
-        // if (result[0].google_id === profile.id) {
-        //
-        // }
-        //
-      }
-      //return cb(err, user);
-      return cb(err, profile.id);
+
+
+
+
+    User.findOrCreate({ googleID: profile.id }, function (err, user) {
+      return cb(err, user);
     });
   }
 ));
 
-// call the facebook strategy
 passport.use(new FacebookStrategy({
     clientID: process.env.APP_ID,
     clientSecret: process.env.APP_SECRET,
     callbackURL: "http://localhost:3000/auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    const findUser = "SELECT google_id FROM login_info WHERE facebook_id = '" + profile.id + "'";
-    //console.log(findUser);
-    con.query(findUser, function(err, result, field) {
-      if (err) throw err;
-
-      console.log(result);
-      if (result.length === 0) {
-        con.query("INSERT INTO login_info (facebook_id) VALUES ('" + profile.id + "')", function(err, result, field) {
-          if (err) throw err;
-        });
-      } else {
-        // if (result[0].google_id === profile.id) {
-        //
-        // }
-        //
-      }
-      //return cb(err, user);
-      return cb(err, profile.id);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
     });
   }
 ));
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
 
 //the get route for google auth
 app.get("/auth/google",
@@ -312,22 +211,9 @@ app.get("/signup", function(req, res) {
   res.render("signup");
 });
 
-// test route
-// app.get("/test/:postName", function(req, res) {
-//   const userEmail = req.params.postName;
-//   console.log(userEmail);
-//   con.query("SELECT vercode FROM login_info", function(err, result, field) {
-//     if (err) throw err;
-//     else {
-//       console.log(result[0].password);
-//       if (result[0].password === 10) {
-//         console.log("it is equal");
-//       } else {
-//         console.log("it is not equal");
-//       }
-//     }
-//   })
-// });
+app.get("/success", function(req,res){
+  res.render("success");
+})
 
 // content route after successfully login or authenticated by Oauth
 app.get("/content", function(req, res) {
@@ -341,51 +227,49 @@ app.get("/content", function(req, res) {
 
 // the route for 2fa
 app.get("/2fa/:postName", function(req, res) {
-  const authEmail = req.params.postName;
-  let verCodeRandom = 0
-  while (verCodeRandom < 99999) {
-    verCodeRandom = Math.round(Math.random() * 1000000);
-  }
-  //   const date_ob = new Date();
-  // // current date
-  // // adjust 0 before single digit date
-  // let date = ("0" + date_ob.getDate()).slice(-2);
-  // // current month
-  // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-  // // current year
-  // let year = date_ob.getFullYear();
-  // // current hours
-  // let hours = date_ob.getHours();
-  // // current minutes
-  // let minutes = date_ob.getMinutes();
-  // // current seconds
-  // let seconds = date_ob.getSeconds();
-  // // prints date in YYYY-MM-DD format
-  // // prints date & time in YYYY-MM-DD HH:MM:SS format
-  // const sentTime = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
 
-  //generate random code
-  const sentTime = Math.round(Date.now() / 1000);
-  const userCode = "UPDATE login_info SET vercode =" + verCodeRandom + ", sent_time = " + sentTime + " WHERE email = '" + authEmail + "'";
-  console.log(userCode);
-  // store the verification code and timestamp into database
-  con.query(userCode, function(err, result, field) {
-    if (err) throw err;
-    else {
-      console.log("vercode successfully inserted to database");
+
+  if (req.isAuthenticated()) {
+    const authEmail = req.params.postName;
+    let verCodeRandom = 0
+    while (verCodeRandom < 99999) {
+      verCodeRandom = Math.round(Math.random() * 1000000);
     }
-  })
+
+    //generate random code
+    const sentTime = Math.round(Date.now() / 1000);
+    // const userCode = "UPDATE login_info SET vercode =" + verCodeRandom + ", sent_time = " + sentTime + " WHERE email = '" + authEmail + "'";
+    //console.log(userCode);
+    User.updateOne({username: authEmail}, {sentTime: sentTime, verCode: verCodeRandom}, function(err, result) {
+      if (err) throw err;
+
+      res.render("2fa", {
+        emailAddress: authEmail
+      });
+    });
+  } else{
+    res.redirect("/login");
+  }
+
+
+  // store the verification code and timestamp into database
+  // con.query(userCode, function(err, result, field) {
+  //   if (err) throw err;
+  //   else {
+  //     console.log("vercode successfully inserted to database");
+  //   }
+  // })
   //send code to phone using twillo api
-  client.messages
-    .create({
-      body: "Your verification code is: " + verCodeRandom + ". Please do not share with other people. The code will expire in 10 minutes.",
-      from: '+17657538135',
-      to: '+12015771959'
-    })
-    .then(message => console.log(message.status));
-  res.render("2fa", {
-    emailAddress: authEmail
-  });
+  // client.messages
+  //   .create({
+  //     body: "Your verification code is: " + verCodeRandom + ". Please do not share with other people. The code will expire in 10 minutes.",
+  //     from: '+17657538135',
+  //     to: '+12015771959'
+  //   })
+  //   .then(message => console.log(message.status));
+  // res.render("2fa", {
+  //   emailAddress: authEmail
+  // });
 });
 
 // post route for user sending back the verification code they recieve
@@ -393,14 +277,14 @@ app.post("/2fa", function(req, res) {
   console.log(req.body.verCode.join(""));
   console.log(req.body.email);
   const postEmail = req.body.email;
-  const getCode = "SELECT vercode, sent_time FROM login_info WHERE email = '" + postEmail + "'"
+  //const getCode = "SELECT vercode, sent_time FROM login_info WHERE email = '" + postEmail + "'"
   const verificationCode = req.body.verCode.join("");
 
-  // get the verification code from the database and check with the code entered by user
-  con.query(getCode, function(err, result, field) {
-    if (err) throw err;
-    const vercodeInDB = result[0].vercode;
-    const timeSent = result[0].sent_time;
+User.findOne({username: postEmail}, function(err, result) {
+  if (err) throw err;
+  console.log(result);
+  const vercodeInDB = result.verCode;
+    const timeSent = result.sentTime;
     const timeDiff = Math.round(Date.now() / 1000) - timeSent;
     console.log(timeDiff);
     if (timeDiff < 600) {
@@ -408,79 +292,80 @@ app.post("/2fa", function(req, res) {
         res.redirect("/content");
       }
     }
-  })
+});
+
+
+  // get the verification code from the database and check with the code entered by user
+  // con.query(getCode, function(err, result, field) {
+  //   if (err) throw err;
+  //   const vercodeInDB = result[0].vercode;
+  //   const timeSent = result[0].sent_time;
+  //   const timeDiff = Math.round(Date.now() / 1000) - timeSent;
+  //   console.log(timeDiff);
+  //   if (timeDiff < 600) {
+  //     if (verificationCode === vercodeInDB) {
+  //       res.redirect("/content");
+  //     }
+  //   }
+  // })
 });
 
 // post route for login comparing the hash to check if they can match
 app.post("/login", function(req, res) {
-  const email = req.body.email;
+  const email = req.body.username;
   const password = req.body.password;
-  const validation = "SELECT password FROM login_info WHERE email = '" + email + "'";
-  con.query(validation, function(err, result, fields) {
-    if (err) {
+  //const validation = "SELECT password FROM login_info WHERE email = '" + email + "'";
+
+  const user = new User({
+    username: email,
+    password: password
+  });
+  req.login(user, function(err){
+    if (err){
       console.log(err);
-    }
-    if (result.length != 0) {
-      if (bcrypt.compare(password, result[0].password)) {
-        res.redirect("/2fa/" + email);
-      }
     } else {
-      res.redirect('/login?error=' + encodeURIComponent('Incorrect_Credential'));
+      console.log("successfully")
+      passport.authenticate("local") (req,res, function(){
+        console.log("check")
+        res.redirect("/2fa/" + email);
+      });
     }
-  })
+  });
+
+
+
 });
 
 // post route for signup
-app.post("/signup", userExists, (req, res, next) => {
+app.post("/signup", function(req,res){
   console.log(req.body.firstName);
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
-  const email = req.body.email;
+  const email = req.body.username;
   const phone = req.body.phone;
   const password = req.body.password;
 
-  //const saltHash = genPassword(password);
-  //console.log(saltHash);
-  //const saltNew = saltHash.salt;
-  //const hashNew = saltHash.hash;
-
-  // generate hash for the password
-  bcrypt.hash(password, salt, function(err, hash) {
-    if (err) {
-      console.log(err);
-    }
-    // store the user information into the sql database
-    const newUser = "INSERT INTO login_info (email, firstName, lastName, phoneNumber, password, salt) VALUES (?,?,?,?,?,?)";
-    console.log(newUser);
-    con.query(newUser, [email, firstName, lastName, phone, hash, salt], function(err, result, fields) {
-      if (err) throw err;
-      console.log("new user added ");
-    });
-    res.redirect("/2fa/" + email);
+  const newUser = new User({
+    email: email,
+    firstName: firstName,
+    lastName: lastName,
+    phone: phone,
+    password: password
   });
 
-  // bcrypt.hash(password, salt, function(err, hash) {
-  //     console.log(salt);
-  //     console.log(hash);
-  // })
-  // passport.use("local-signup", new LocalStrategy({
-  //     usernameField: email,
-  //     passwordField: password,
-  //     passReqToCallback: true
-  //   },
-  //   function(req, email, password, done) {
-  //     console.log("this is a check makr");
-  //     con.query("SELECT email FROM login_info WHERE email = '" + email + "'", function(err, result, field) {
-  //       console.log(result);
-  //       if (err) return done(err);
-  //       if (result.length != 0) {
-  //         var newUserMysql = new Object();
-  //
-  //         newUserMysql.email = email;
-  //         newUserMysql.password = password; //
-  //       }
-  //     })
-  //   }))
+
+  User.register({username: email, firstName: firstName, lastName: lastName, phone: phone}, password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        console.log("went through")
+        res.redirect("/success");
+      });
+    }
+  });
+
 });
 
 // listenning to port 3000
